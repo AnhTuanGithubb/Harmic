f99d3d7623275b0f006ac2292d1dbb15c2df945c
AnhTuanGithubb <anh.tuan.nguyen20072004@gmail.com>
2024-11-18 14:17:06 +0700

    ADD BAITH10 - quản trị QL Sản phẩm ( +Areas\Admin/products (index.cshtml, create.cshtml,edit.cshtml,delete.cshtml))

--- Harmic/ViewComponents/ProductViewComponent.cs
+++ Harmic/ViewComponents/ProductViewComponent.cs
@@ -16,7 +16,7 @@ namespace Harmic.ViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var items = _context.TbProducts.Include(m => m.CategoryProduct)
-                .Where(m => (bool)m.IsActive).Where(m => m.IsActive);
+                .Where(m => (bool)m.IsActive).Where(m => m.IsNew);
             return await Task.FromResult<IViewComponentResult>
                 (View(items.OrderByDescending(m => m.ProductId).ToList()));
         }
